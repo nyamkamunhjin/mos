d2baf49da0f5c19095b940973e61263b2bd41360
Munkhjin Nyamdorj <nyamkamunhjin@gmail.com>
2026-05-28 02:29:20 +0800

    switch bird grid to 3 columns

--- app/birds/page.tsx
+++ app/birds/page.tsx
@@ -274,7 +274,7 @@ function BirdsPageContent() {
                 )}
               </div>
             ) : (
-              <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                 {birds.map((bird) => (
                   <BirdCard key={bird.id} bird={bird} />
                 ))}

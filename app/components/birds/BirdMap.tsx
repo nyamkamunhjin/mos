b5f88cf43930afc5aabf1254a661d57ecce92b69
Munkhjin Nyamdorj <nyamkamunhjin@gmail.com>
2026-05-28 02:15:39 +0800

    switch OSM tiles to CartoDB Positron to remove traditional Mongolian script labels from bird maps

--- app/components/birds/BirdMap.tsx
+++ app/components/birds/BirdMap.tsx
@@ -54,8 +54,8 @@ export default function BirdMap({
         className="w-full h-full"
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/">CARTO</a>'
+          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
         />
         {locations.map((loc, i) => (
           <Marker key={i} position={[loc.lat, loc.lng]} icon={defaultIcon}>
